deab286bc4552e166549ef76ec91a2fc579de9ac
Manar <elsaidmanar@gmail.com>
2024-01-01 02:48:20 -0800

    2

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,7 +22,8 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def displaytext():
     """display C  followed by the value of the text variable"""
-    return ("C{}".format(text.replace("_", " ")))
+    return ("C {}".format(text.replace("_", " ")))
+
 
 
 if __name__ == "__main__":
