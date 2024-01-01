04c8152a57ad67497ba856366467606545cc2fa7
Manar <elsaidmanar@gmail.com>
2024-01-01 09:57:21 -0800

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """return Hello HBNB! """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """return HBNB"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """display C  followed by the value of the text variable"""
+    return ("C {}".format(text.replace("_", " ")))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text="is cool"):
+    """display Python, followed by the value of the text variabl"""
+    return ("Python {}".format(text.replace("_", " ")))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """ display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return ("n is a number")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
