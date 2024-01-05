1cd5576159bbd30ce455c1a1b99adfc4c7372068
Manar Elsaid <elsaidmanar@gmail.com>
2024-01-05 19:37:52 +0000

    8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask, render _template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states:
+  """render html page that display states"""
+  states = storage.all()
+  return render_template("7-states_list.html", states = states)
+
+
+@app.teardown_appcontext
+def teardown_appcontext:
+  """ remove the current SQLAlchemy Session"""
+  storage.close()
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
