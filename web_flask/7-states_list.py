558a60a8ed6d7168d48335ee4fd80542a08cbfdb
ManarElsaid <elsaidmanar@gmail.com>
2024-01-09 13:24:48 +0200

    pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,15 +10,16 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
-  """render html page that display states"""
-  states = storage.all(State)
-  return render_template("7-states_list.html", states = states)
+    """render html page that display states"""
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
-  """ remove the current SQLAlchemy Session"""
-  storage.close()
+    """ remove the current SQLAlchemy Session"""
+    storage.close()
+
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")
